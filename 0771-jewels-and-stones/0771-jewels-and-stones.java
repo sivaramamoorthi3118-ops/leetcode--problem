c0a15ac910a8544c1d9bc40b5c3b7da77d1d9e9e
sivaramamoorthi3118-ops <sivaramamoorthi3118@gmail.com>
2026-07-22 13:46:41 +0530

    Time: 0 ms (100%), Space: 43.2 MB (54.86%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+
+        for (char ch : stones.toCharArray()) {
+            if (jewels.indexOf(ch) != -1) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
